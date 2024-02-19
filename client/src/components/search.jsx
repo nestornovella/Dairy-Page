57051c3adfe4799fc5c2dfcf4b33b965955ca864
nestornovella <importarnorth@gmail.com>
2024-02-19 13:41:45 -0300

    whatsapp implements

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -21,6 +21,7 @@ function Search({ callback }) {
             <img style={{maxWidth:"500px"}} src="https://vendiendoofertas.files.wordpress.com/2016/07/logo-vendiendo-ofertas-fondo-transparente1.png" alt="" />
             <div className="col-sm-12 col-xl-9 bg-dark p-3 rounded-4">
                 <div className="form-group d-flex">
+                <a href="https://api.whatsapp.com/send?phone=1125420570"><img width={50} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="" /></a>
                     <input ref={inputValue} type="text"
                         className="form-control mx-2" name="" id="" aria-describedby="helpId" placeholder="" />
                     <button onClick={() => setValue()} type="button" className="btn btn-outline-primary mx-2">buscar</button>
