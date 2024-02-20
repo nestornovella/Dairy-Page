dcf2b6415aae516256e6973d42eb2580c4ab1c04
nestornovella <importarnorth@gmail.com>
2024-02-20 15:02:42 -0300

    add cart

--- client/src/components/categorySection.jsx
+++ client/src/components/categorySection.jsx
@@ -5,13 +5,13 @@ function CategorySection({current, newProducts, lacteos, perfumeria}) {
     return (  
         <div className="row mt-3 rounded-4 ">
             <div className="col-xl-4 my-1">
-            <CategoryCard current={current} callBack={lacteos} color={"#89cff0"} title={"LACTEOS"}/>
+            <CategoryCard current={current} callBack={lacteos} color={"#5EE8B6"} title={"LACTEOS"}/>
             </div>
             <div className="col-xl-4 my-1">
             <CategoryCard current={current} callBack={perfumeria} color={"#86b6f4"} title={"PERFUMERIA"}/>
             </div>
             <div className="col-xl-4 my-1">
-            <CategoryCard current={current} callBack={newProducts} color={"#ff6961"} title={"NUEVOS"}/>
+            <CategoryCard current={current} callBack={newProducts} color={"#C67A2F"} title={"NUEVOS"}/>
             </div>
         </div>
     );
