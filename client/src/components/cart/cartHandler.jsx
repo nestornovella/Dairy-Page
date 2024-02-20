de741a9b92a59ee6903b367fd9a44b8a8a393967
nestornovella <importarnorth@gmail.com>
2024-02-20 18:21:01 -0300

    changes

--- client/src/components/cart/cartHandler.jsx
+++ client/src/components/cart/cartHandler.jsx
@@ -2,28 +2,48 @@ import React, { useEffect, useRef, useState } from "react";
 import Cart from "./cart";
 import carritoImagen from "../../assets/carrito.png"
 import './../../App.css';
+import { useSelector } from "react-redux";
 
 function CartHandler({ pedido, deleteProductCart }) {
+    const cart = useSelector(store => store.cart)
+
     const [showCart, setShowCart] = useState(false);
+    const [updated, setUpdated] = useState(true)
     let handler = useRef(null)
 
-    useEffect(()=>{
-        if(showCart){
+
+    useEffect(() => {
+        if (showCart) {
             handler.current.className = "row showCart"
-        }else{
+        } else {
             handler.current.className = "row dontShowCart"
         }
         console.log(handler.current)
-    },[showCart])
+    }, [showCart])
 
-    function handleCart(){
+    function handleCart() {
         setShowCart(prevShowCart => !prevShowCart)
     }
+
+    useEffect(()=>{
+        
+            setUpdated(true)
+            console.log(updated)
+            setTimeout(()=>{
+            setUpdated(false)
+            console.log(updated)
+        },600)
+    },[cart])
 
 
     return (
         <div>
-            <div className="row cartOpenButton"><button onClick={handleCart} style={{backgroundColor:"transparent", border:"none", borderRadius:"10%",}}><img width={200} src={carritoImagen} alt="" /></button></div>
+            <div className="row cartOpenButton">
+                <button onClick={handleCart} style={{ backgroundColor: "transparent", border: "none", borderRadius: "10%", }}>
+                    <img width={200} src={carritoImagen} alt="" />
+                </button>
+                {cart.length &&<button className={`cartCounter ${updated ? "updatedCart" : ''}`}>{cart.length}</button>}
+            </div>
             <div ref={handler} className="row dontShowCart">
                 <Cart handleCart={handleCart} deleteProductCart={deleteProductCart} data={pedido} />
             </div>
