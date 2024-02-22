543db4ca37e6c72389165a47c9ca01f6d4b5e9f6
nestornovella <importarnorth@gmail.com>
2024-02-22 12:31:20 -0300

    changes bar nav

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -20,10 +20,9 @@ function Search({ callback }) {
         <div className="row d-flex justify-content-center mt-2 mb-0" >
             <img style={{maxWidth:"500px"}} src="https://vendiendoofertas.files.wordpress.com/2016/07/logo-vendiendo-ofertas-fondo-transparente1.png" alt="" />
             <div className="col-sm-12 col-xl-9  p-3 rounded-4" style={{backgroundColor:"#C62271"}}>
-                <div className="form-group d-flex">
-                <a href="https://api.whatsapp.com/send?phone=+541125420570&text=hola"><img width={50} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" alt="" /></a>
+                <div className="form-group barraBusqueda">
                     <input ref={inputValue} type="text"
-                        className="form-control mx-2" name="" id="" aria-describedby="helpId" placeholder="" />
+                        className="form-control mx-2"  name="" id="" aria-describedby="helpId" placeholder="" />
                     <button onClick={() => setValue()} type="button" className="btn btn-dark mx-2">buscar</button>
                     <button onClick={() => cleanInput()} type="button" className="btn btn-dark mx-2"><img width={30} src="https://cdn-icons-png.freepik.com/512/3221/3221897.png" alt="" /></button>
                 
