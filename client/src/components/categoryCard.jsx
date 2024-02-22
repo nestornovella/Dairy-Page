ae7a62820968d0a6fb876808a937f932ffb4c636
nestornovella <importarnorth@gmail.com>
2024-02-22 17:44:50 -0300

    apply styles

--- client/src/components/categoryCard.jsx
+++ client/src/components/categoryCard.jsx
@@ -5,7 +5,7 @@ function CategoryCard({ current, title, callBack, color }) {
     const isActive = current.toLowerCase() === title.toLowerCase();
 
     return (
-        <button onClick={callBack} className={` w-100 categoryCard ${isActive ? 'active' : ''}`} style={{ backgroundColor: color }}>
+        <button onClick={callBack} className={` w-100 categoryCard ${isActive ? 'active' : ''}`} style={{ backgroundColor: color}}>
             <h2 className="font-weight-bolder">{title}</h2>
         </button>
     );
--- client/src/components/categorySection.jsx
+++ client/src/components/categorySection.jsx
@@ -3,12 +3,12 @@ import CategoryCard from "./categoryCard";
 
 function CategorySection({current, newProducts, lacteos, perfumeria}) {
     return (  
-        <div className="row mt-3 rounded-4 ">
+        <div className="row mt-4 rounded-4  ">
             <div className="col-xl-4 my-1">
-            <CategoryCard current={current} callBack={lacteos} color={"#5EE8B6"} title={"LACTEOS"}/>
+            <CategoryCard current={current} callBack={lacteos} color={"#F934FF"} title={"LACTEOS"}/>
             </div>
             <div className="col-xl-4 my-1">
-            <CategoryCard current={current} callBack={perfumeria} color={"#86b6f4"} title={"PERFUMERIA"}/>
+            <CategoryCard current={current} callBack={perfumeria} color={"#2488BA"} title={"PERFUMERIA"}/>
             </div>
             <div className="col-xl-4 my-1">
             <CategoryCard current={current} callBack={newProducts} color={"#C67A2F"} title={"NUEVOS"}/>
