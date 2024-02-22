40ddfaa3ee5d1b3da6e9ff0c0b43322e4743caa1
nestornovella <importarnorth@gmail.com>
2024-02-22 12:50:02 -0300

    changes nar nav

--- client/src/hooks/templateWhatsapp.js
+++ client/src/hooks/templateWhatsapp.js
@@ -3,18 +3,20 @@ import { useSelector } from "react-redux";
 function useSendData() {
     const cart = useSelector(store => store.cart);
     const totalCart = useSelector(store => store.totalCart);
+    const date = new Date()
+    const month = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    const actualDate = date.getDay() + " " + month[+date.getMonth()]+ " " + date.getFullYear()
 
-    let template = '::::::PEDIDO::::::%0A';
 
-    console.log(cart);
 
+    let template = `::::::PEDIDO ${actualDate}::::::%0A`;
     cart.forEach(prod => {
-        template += ` %0APRODUCTO: ${prod.variety ? (prod.name + " " + prod.variety[prod.selected] ): prod.name} CANTIDAD: ${prod.cantidad} SUBTOTAL: ${prod.subTotal}%0A `;
-        
+        template += ` %0APRODUCTO: ${prod.variety ? (prod.name + " " + prod.variety[prod.selected]) : prod.name} CANTIDAD: ${prod.cantidad} SUBTOTAL: ${prod.subTotal}%0A `;
+
     });
 
-    template += `-----------------------------------------------------------%0ATotal: ${totalCart}` 
-    
+    template += `-----------------------------------------------------------%0ATotal: ${totalCart}`
+
 
     return template;
 }
