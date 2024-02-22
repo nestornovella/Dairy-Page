047f1bc56e4b782040dab562530c6d18ef1449c1
nestornovella <importarnorth@gmail.com>
2024-02-22 18:32:10 -0300

    changes

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-
+import enviosLogo from "../assets/enviosLogo.png"
 
 
 function Search({ callback }) {
@@ -10,22 +10,26 @@ function Search({ callback }) {
         callback(inputValue.current.value)
     }
 
-    function cleanInput(){
+    function cleanInput() {
         inputValue.current.value = ""
         callback(inputValue.current.value)
     }
 
 
     return (
-        <div className="row d-flex justify-content-center mt-2 mb-0" >
-            <img style={{maxWidth:"500px"}} src="https://vendiendoofertas.files.wordpress.com/2016/07/logo-vendiendo-ofertas-fondo-transparente1.png" alt="" />
-            <div className="col-sm-12 col-xl-9  p-3 rounded-4" style={{backgroundColor:"#C62271"}}>
+        <div className="row d-flex justify-content-center mt-3 mb-0" >
+            <div className="row mb-5 d-flex justify-content-center">
+                <img style={{ maxWidth: "500px" }} src="https://vendiendoofertas.files.wordpress.com/2016/07/logo-vendiendo-ofertas-fondo-transparente1.png" alt="" />
+            </div>
+            <div className="col-sm-12 col-xl-9  p-3 rounded-4" style={{ backgroundColor: "#C62271", position: "relative" }}>
                 <div className="form-group barraBusqueda">
                     <input ref={inputValue} type="text"
-                        className="form-control  mx-2"  name="" id="" aria-describedby="helpId" placeholder="" />
+                        className="form-control  mx-2" name="" id="" aria-describedby="helpId" placeholder="" />
                     <button onClick={() => setValue()} type="button" className="btn btn-dark mx-2"><img width={30} src="https://cdn-icons-png.flaticon.com/512/4305/4305549.png" alt="" /></button>
                     <button onClick={() => cleanInput()} type="button" className="btn btn-dark mx-2"><img width={30} src="https://cdn-icons-png.freepik.com/512/3221/3221897.png" alt="" /></button>
-                
+                    <div className="row enviosLogo">
+                        <img src={enviosLogo} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
