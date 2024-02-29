bc5103336812de7b55175483eb2ff9c0d87ee800
nestornovella <importarnorth@gmail.com>
2024-02-29 09:04:40 -0300

    changes

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -21,7 +21,7 @@ function Search({ callback }) {
             <div className="row mb-5 d-flex justify-content-center initialLogo">
                 <img style={{ maxWidth: "500px" }} src="https://vendiendoofertas.files.wordpress.com/2016/07/logo-vendiendo-ofertas-fondo-transparente1.png" alt="" />
             </div>
-            <div className="col-sm-12 col-xl-9  p-3 rounded-4" style={{ backgroundColor: "#C62271", position: "relative" }}>
+            <div className="col-sm-12 col-xl-9  p-3 rounded-4 barraBusquedaContainer" >
                 <div className="form-group barraBusqueda">
                     <input ref={inputValue} type="text"
                         className="form-control  mx-2" name="" id="" aria-describedby="helpId" placeholder="" />
