277a11eafdb6cd4c35000026d88297ad1bf8e2c1
Nestor Novella <89081612+nestornovella@users.noreply.github.com>
2024-03-08 05:54:35 -0500

    Actualizar categorySection.jsx

--- client/src/components/categorySection.jsx
+++ client/src/components/categorySection.jsx
@@ -5,10 +5,10 @@ function CategorySection({current, newProducts, lacteos, perfumeria}) {
     return (  
         <div className="row mt-4 rounded-4  ">
             <div className="col-xl-4 my-1">
-            <CategoryCard current={current} callBack={lacteos} color={"#29A850"} title={"LACTEOS"}/>
+            <CategoryCard current={current} callBack={lacteos} color={"#2488ba"} title={"LACTEOS"}/>
             </div>
             <div className="col-xl-4 my-1">
-            <CategoryCard current={current} callBack={perfumeria} color={"#2488BA"} title={"PERFUMERIA"}/>
+            <CategoryCard current={current} callBack={perfumeria} color={"#29a850"} title={"PERFUMERIA"}/>
             </div>
             <div className="col-xl-4 my-1">
             <CategoryCard current={current} callBack={newProducts} color={"#C67A2F"} title={"NUEVOS"}/>
