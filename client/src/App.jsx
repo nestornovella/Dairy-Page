7ae094512cfb4f40ceb1352aef9d228fe8830e08
nestornovella <importarnorth@gmail.com>
2024-03-18 12:38:17 -0300

    adding products

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,76 +4,76 @@ import './App.css';
 import CategorySection from './components/categorySection';
 import ProductSection from './components/product/productSection';
 import { useEffect, useState } from 'react';
-import comestibles from"./data/comestibles.json"
-import noComestibles from"./data/noComestibles.json"
+import comestibles from "./data/comestibles.json"
+import noComestibles from "./data/noComestibles.json"
 import CartHandler from './components/cart/cartHandler';
 import useSendData from './hooks/templateWhatsapp';
 import Entrega from './components/fechaDeEntrega';
 
 
 function App() {
-  const [data, setData]= useState([])
-  const [category, setCategory]= useState("lacteos")
-  const [pedido, setPedido]= useState([])
-  
+  const [data, setData] = useState([])
+  const [category, setCategory] = useState("lacteos")
+  const [pedido, setPedido] = useState([])
+
   useSendData()
-  
-  useEffect(()=>{
-    if(category === 'lacteos'){
+
+  useEffect(() => {
+    if (category === 'lacteos') {
       setData(comestibles)
-    }else if(category === "perfumeria"){
+    } else if (category === "perfumeria") {
       setData(noComestibles)
-    }else{
+    } else {
       setData([...comestibles, ...noComestibles].filter(prod => prod.isNew))
     }
 
-  },[category])
+  }, [category])
 
-  function lacteos(){
+  function lacteos() {
     setCategory("lacteos")
   }
 
-  function perfumeria(){
+  function perfumeria() {
     setCategory("perfumeria")
   }
 
-  function newProducts(){
+  function newProducts() {
     setCategory("nuevos")
   }
-  function search(param){
-    if(param){
+  function search(param) {
+    if (param) {
       setData(() => [...comestibles, ...noComestibles].filter(prod => prod.name.toLowerCase().includes(param.toLowerCase())))
-    }else{
-      if(category === 'lacteos'){
+    } else {
+      if (category === 'lacteos') {
         setData(comestibles)
-      }else{
+      } else {
         setData(noComestibles)
       }
     }
   }
 
-  function addToCart(product){
+  function addToCart(product) {
     let found = false
     pedido.forEach(prod => prod.name === product.name ? found = true : "")
 
-    !found && 
-    setPedido(prev => [...prev, product])
+    !found &&
+      setPedido(prev => [...prev, product])
   }
 
-  function deleteProductCart(name){
+  function deleteProductCart(name) {
     setPedido(prev => [...prev].filter(prod => !prod.name.includes(name)))
   }
 
   return (
-    
+    <>
+      <Entrega />
       <div className="container">
-        <Entrega/>
-        <CartHandler  deleteProductCart={deleteProductCart} pedido={pedido}/>
-        <Search callback={search}/>
-        <CategorySection  current={category} newProducts={newProducts} lacteos={lacteos} perfumeria={perfumeria} />
+        <CartHandler deleteProductCart={deleteProductCart} pedido={pedido} />
+        <Search callback={search} />
+        <CategorySection current={category} newProducts={newProducts} lacteos={lacteos} perfumeria={perfumeria} />
         <ProductSection addProduct={addToCart} data={data} />
       </div>
-    
+    </>
 
 
   )
