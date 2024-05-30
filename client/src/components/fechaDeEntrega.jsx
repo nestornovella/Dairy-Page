f45112beaa0f3b068823a4905888d3ba7a2dce77
nestornovellafullstackdev <nestornovellafullstackdev@gmail.com>
2024-05-30 01:47:43 +0000

    changes idx

--- client/src/components/fechaDeEntrega.jsx
+++ client/src/components/fechaDeEntrega.jsx
@@ -1,11 +1,23 @@
-import fecha from "../data/fechaDeEntrega.json"
+
+
 
 
 function Entrega() {
+
+    const currentDate = () => {
+        const meses = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic']
+        const currDate = new Date()
+        // quiero obtener el proximo dia lunes
+        const nextMonday = new Date(currDate.setDate(currDate.getDate() + (1 + 7 - currDate.getDay()) % 7));
+        const date = nextMonday.getDate().length > 1 ? nextMonday.getDate() : `0${nextMonday.getDate()}`;
+        const month = meses[nextMonday.getMonth()];
+        return `Lunes ${date} ${month}`
+    }
+    
     return (
         <div className="row fechaEntregaContainer ">
-            {fecha.active && <p className="m-0">se toma pedidos para el
-                <span style={{ color: 'yellow', fontSize: "25px" }}>👉  {fecha.date}</span>
+            {<p className="m-0">se toma pedidos para el
+                <span style={{ color: 'yellow', fontSize: "18px" }}>👉  {currentDate()}</span>
             </p>}
         </div>
     );
