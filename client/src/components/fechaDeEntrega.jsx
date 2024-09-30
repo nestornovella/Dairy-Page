eade24fd486ad10357996a7c646bc913ae6113bd
nestornovellafullstackdev <nestornovellafullstackdev@gmail.com>
2024-09-30 17:16:39 +0000

    changes in date

--- client/src/components/fechaDeEntrega.jsx
+++ client/src/components/fechaDeEntrega.jsx
@@ -7,11 +7,11 @@ function Entrega() {
     const currentDate = () => {
         const meses = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic']
         const currDate = new Date()
-        // quiero obtener el proximo dia lunes
         const nextMonday = new Date(currDate.setDate(currDate.getDate() + (1 + 7 - currDate.getDay()) % 7));
         const date = nextMonday.getDate().length > 1 ? nextMonday.getDate() : `0${nextMonday.getDate()}`;
         const month = meses[nextMonday.getMonth()];
         return `Lunes ${date} ${month}`
+
     }
     
     return (
